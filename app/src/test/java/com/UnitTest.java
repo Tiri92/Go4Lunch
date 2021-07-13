d6767c66ed0554ad3af0fdacb96b81fc91009221
Thierry Bitar <bitarthierry@gmail.com>
2021-07-13 21:57:55 +0200

    Update of principal package name

--- app/src/test/java/com/UnitTest.java
+++ app/src/test/java/com/UnitTest.java
@@ -1,4 +1,4 @@
-package com.example.go4lunch;
+package com;
 
 import org.junit.Test;
 
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class UnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
