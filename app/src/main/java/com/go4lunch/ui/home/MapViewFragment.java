002193ea203be5b2318547918be1340af56e0e2f
Thierry Bitar <bitarthierry@gmail.com>
2021-07-19 10:27:18 +0200

    Get the location of the user and display it on the GoogleMap

--- app/src/main/java/com/go4lunch/ui/home/MapViewFragment.java
+++ app/src/main/java/com/go4lunch/ui/home/MapViewFragment.java
@@ -1,28 +1,65 @@
 package com.go4lunch.ui.home;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.go4lunch.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static androidx.core.content.ContextCompat.getSystemService;
 
 public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationCallback locationCallback;
+    LatLng myPosition;
 
-    @Nullable
+    private static final int LOCATION_REQUEST_INTERVAL_MS = 10_000;
+    private static final float SMALLEST_DISPLACEMENT_THRESHOLD_METER = 25;
+
+    @SuppressLint("MissingPermission")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
@@ -34,26 +71,70 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
             mapFragment.getMapAsync(this);
         }
 
+        startLocationRequest();
+
         return root;
+    }
+
+    @RequiresPermission(anyOf = {"android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"})
+    private void startLocationRequest() {
+        ActivityCompat.requestPermissions(
+                requireActivity(),
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0
+        );
+
+        if (locationCallback == null) {
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    Location location = locationResult.getLastLocation();
+                    myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                    if (mMap != null) {
+                        moveAndDisplayMyPosition();
+                    }
+                }
+            };
+        }
+
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(requireActivity().getApplication());
+
+        client.removeLocationUpdates(locationCallback);
+
+        client.requestLocationUpdates(
+                LocationRequest.create()
+                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                        .setSmallestDisplacement(SMALLEST_DISPLACEMENT_THRESHOLD_METER)
+                        .setInterval(LOCATION_REQUEST_INTERVAL_MS),
+                locationCallback,
+                Looper.getMainLooper()
+        );
+
+    }
+
+    private void moveAndDisplayMyPosition() {
+        mMap.addMarker(new MarkerOptions()
+                .position(myPosition)
+                .title("My position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+    }
+
+    public boolean hasLocationPermission() {
+        return ContextCompat.checkSelfPermission(requireActivity().getApplication(), ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng paris = new LatLng(48,12);
-        mMap.addMarker(new MarkerOptions()
-                .position(paris)
-                .title("Marker in Paris"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        if (myPosition == null) {
+            LatLng startPosition = new LatLng(-34, 151); //new LatLng(48, 12);
+            mMap.addMarker(new MarkerOptions()
+                    .position(startPosition)
+                    .title("Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(startPosition));
+        } else {
+            moveAndDisplayMyPosition();
+        }
     }
 
-    /*@Override
-    public void onViewCreated(@NonNull @org.jetbrains.annotations.NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        Toolbar toolbar = view.findViewById(R.id.toolbar);
-    }*/
 }
 
