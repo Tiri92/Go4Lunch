6dbae327752e6b82d31e370b7565e73ccc9fa17b
Thierry Bitar <bitarthierry@gmail.com>
2021-07-21 15:58:33 +0200

    LunchFragment added in Navigation Drawer

---
+++ app/src/main/java/com/go4lunch/ui/main/LunchFragment.java
@@ -0,0 +1,11 @@
+package com.go4lunch.ui.main;
+
+import androidx.fragment.app.Fragment;
+
+import com.go4lunch.R;
+
+public class LunchFragment extends Fragment {
+
+    public LunchFragment() {super(R.layout.nav_fragment_lunch);}
+
+}
