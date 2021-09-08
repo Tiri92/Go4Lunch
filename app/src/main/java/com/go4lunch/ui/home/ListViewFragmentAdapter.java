55fba8fbd4764548a0e013eb99f58cfa20e40b3c
Thierry Bitar <bitarthierry@gmail.com>
2021-09-08 18:16:25 +0200

    Display Photos of restaurants on ListViewFragmentAdapter

--- app/src/main/java/com/go4lunch/ui/home/ListViewFragmentAdapter.java
+++ app/src/main/java/com/go4lunch/ui/home/ListViewFragmentAdapter.java
@@ -1,13 +1,18 @@
 package com.go4lunch.ui.home;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.go4lunch.BuildConfig;
 import com.go4lunch.R;
 import com.go4lunch.model.nearbysearch.ResultsItem;
 
@@ -20,6 +25,7 @@ public class ListViewFragmentAdapter extends RecyclerView.Adapter<ListViewFragme
     View itemView;
     private List<ResultsItem> listOfRestaurants;
     private TextView nameOfRestaurant;
+    private ImageView restaurantPic;
     private TextView openingHour;
 
     public ListViewFragmentAdapter(List<ResultsItem> listOfRestaurants) {
@@ -38,6 +44,24 @@ public class ListViewFragmentAdapter extends RecyclerView.Adapter<ListViewFragme
     public void onBindViewHolder(@NonNull @NotNull ListViewFragmentAdapter.ViewHolder holder, int position) {
         nameOfRestaurant = itemView.findViewById(R.id.restaurant_name);
         nameOfRestaurant.setText(listOfRestaurants.get(position).getName());
+
+        restaurantPic = itemView.findViewById(R.id.restaurant_pic);
+        try {
+            String base = "https://maps.googleapis.com/maps/api/place/photo?";
+            String key = "key=" + BuildConfig.MAPS_API_KEY;
+            String reference = "&photoreference=" + listOfRestaurants.get(position).getPhotos().get(0).getPhotoReference();
+            String maxH = "&maxheight=157";
+            String maxW = "&maxwidth=157";
+            String query = base + key + reference + maxH + maxW;
+
+            Glide.with(restaurantPic)
+                    .load(query)
+                    .centerCrop()
+                    .into(restaurantPic);
+
+        } catch (Exception e) {
+            Log.i("[THIERRY]", "Exception : " + e.getMessage());
+        }
     }
 
     @Override
