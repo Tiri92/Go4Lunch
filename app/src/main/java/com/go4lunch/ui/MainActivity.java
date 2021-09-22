c44ae84ba9e78e7db584fe351724adf294e26c8e
Thierry Bitar <bitarthierry@gmail.com>
2021-09-22 12:08:21 +0200

    Display user profile picture on the Drawer

--- app/src/main/java/com/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/go4lunch/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.go4lunch.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
+import com.go4lunch.BuildConfig;
 import com.go4lunch.R;
 import com.go4lunch.databinding.ActivityMainBinding;
 import com.go4lunch.model.User;
@@ -27,6 +30,8 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -66,8 +71,6 @@ public class MainActivity extends AppCompatActivity {
                 String userEmail = TextUtils.isEmpty(firebaseUser.getEmail()) ? getString(R.string.no_email_found) : firebaseUser.getEmail(); // Condition ternaire
                 TextView userEmailTextView = binding.navView.getHeaderView(0).findViewById(R.id.user_email_field);
                 userEmailTextView.setText(userEmail);
-
-                ImageView userPic = binding.navView.getHeaderView(0).findViewById(R.id.nav_header_imageview); //TODO
             });
             mainActivityViewModel.getUserDataForUpdate().addSnapshotListener(new EventListener<DocumentSnapshot>() {
                 @Override
@@ -80,6 +83,19 @@ public class MainActivity extends AppCompatActivity {
                         String username = TextUtils.isEmpty(user.getUsername()) ? getString(R.string.no_username_found) : user.getUsername(); // Condition ternaire
                         TextView usernameTextView = binding.navView.getHeaderView(0).findViewById(R.id.username_field);
                         usernameTextView.setText(username);
+
+                        ImageView userPic = binding.navView.getHeaderView(0).findViewById(R.id.nav_header_imageview);
+                        try {
+                            String query = user.getUrlPicture();
+
+                            Glide.with(userPic)
+                                    .load(query)
+                                    .circleCrop()
+                                    .into(userPic);
+
+                        } catch (Exception e) {
+                            Log.i("[THIERRY]", "Exception : " + e.getMessage());
+                        }
                     }
                 }
             });
